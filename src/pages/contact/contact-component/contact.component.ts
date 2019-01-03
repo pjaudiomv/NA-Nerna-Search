a247c4812408ffb19bea6a2b377edb34247472f0
pjaudiomv <pjaudiomv@gmail.com>
2019-01-02 22:58:51 -0500

    added loader to contacts page

--- src/pages/contact/contact-component/contact.component.ts
+++ src/pages/contact/contact-component/contact.component.ts
@@ -3,6 +3,7 @@ import { Config }           from '../../../app/app.config';
 import { ServiceGroupsProvider } from '../../../providers/service-groups/service-groups';
 import { InAppBrowser }     from '@ionic-native/in-app-browser';
 import { LoadingController } from 'ionic-angular';
+import { TranslateService }       from '@ngx-translate/core';
 
 @Component({
   selector: 'page-contact',
@@ -18,16 +19,44 @@ export class ContactComponent {
 
     constructor ( private ServiceGroupsProvider : ServiceGroupsProvider,
                   public loadingCtrl            : LoadingController,
-                  private iab          : InAppBrowser,
-                  private config                : Config ) {
+                  private iab                   : InAppBrowser,
+                  private config                : Config,
+                  private translate             : TranslateService ) {
+
+
+        this.translate.get('LOADINGCONTACTS').subscribe(
+            value => {
+                // value is our translated string
+                this.presentLoader(value);
+            }
+        )
         // Contact page constructor
         this.getServiceGroupContactDetails();
+
     }
 
     getServiceGroupContactDetails(){
+        this.dismissLoader();
         this.ServiceGroupsProvider.getAllServiceGroups().subscribe((serviceGroupData)=>{
             this.serviceGroupNames = serviceGroupData;
         });
+
+    }
+
+    presentLoader(loaderText) {
+        if (!this.loader) {
+            this.loader = this.loadingCtrl.create({
+                content: loaderText
+            });
+            this.loader.present();
+        }
+    }
+
+    dismissLoader() {
+        if(this.loader){
+            this.loader.dismiss();
+            this.loader = null;
+        }
     }
 
   public openLink(url) {
