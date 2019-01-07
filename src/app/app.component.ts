c5f8ee549144a86489483b4e4fc1d95a48a191e1
pjaudiomv <pjaudiomv@gmail.com>
2019-01-07 12:30:22 -0500

    hide wordpress posts for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component,
-	       ViewChild }               from '@angular/core';
+           ViewChild }               from '@angular/core';
 import { Nav,
-	       Platform,
-				 MenuController }          from 'ionic-angular';
+           Platform,
+                 MenuController }          from 'ionic-angular';
 import { StatusBar }               from '@ionic-native/status-bar';
 import { SplashScreen }            from '@ionic-native/splash-screen';
 import { Storage }                 from '@ionic/storage';
@@ -20,64 +20,64 @@ import { AddressSearchComponent}   from '../pages/address-search/address-search-
 import { ContactComponent }        from '../pages/contact/contact-component/contact.component';
 import { AboutComponent }        from '../pages/about/about-component/about.component';
 import { EventsComponent } from '../pages/events/events-component/events.component';
-import { WordpressPosts } from '../pages/wordpress/wordpress-posts/wordpress-posts.component';
+//import { WordpressPosts } from '../pages/wordpress/wordpress-posts/wordpress-posts.component';
 
 @Component({
-	templateUrl: './app.html'
+    templateUrl: './app.html'
 })
 export class MyApp {
-	@ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-	rootPage = TabsComponent;
-	pages: Array<{title: string, component: any, icon: string}>;
+    rootPage = TabsComponent;
+    pages: Array<{title: string, component: any, icon: string}>;
 
 
-	constructor(
-		private platform        : Platform,
-		private translate       : TranslateService,
-		private storage         : Storage,
-		private statusBar       : StatusBar,
-		private splashScreen    : SplashScreen,
-		private config          : Config,
-		private menuController  : MenuController
-		) {
-		this.initializeApp();
+    constructor(
+        private platform        : Platform,
+        private translate       : TranslateService,
+        private storage         : Storage,
+        private statusBar       : StatusBar,
+        private splashScreen    : SplashScreen,
+        private config          : Config,
+        private menuController  : MenuController
+        ) {
+        this.initializeApp();
 
-		this.translate.setDefaultLang('en');
-		storage.get('language').then((value) => {
-			if (value) {
-				this.translate.use(value);
-			} else {
-				this.translate.use('en');
-				storage.set('language', 'en');
-			}
-		});
+        this.translate.setDefaultLang('en');
+        storage.get('language').then((value) => {
+            if (value) {
+                this.translate.use(value);
+            } else {
+                this.translate.use('en');
+                storage.set('language', 'en');
+            }
+        });
 
-		this.pages = [
+        this.pages = [
             { title: 'HOME',           component: TabsComponent,           icon: 'home' },
-			{ title: 'MAP_SEARCH',     component: MapSearchComponent,      icon: 'map'},
-			{ title: 'LOCATIONSEARCH', component: LocationSearchComponent, icon: 'pin' },
-			{ title: 'ADDRESSSEARCH',  component: AddressSearchComponent,  icon: 'compass' },
+            { title: 'MAP_SEARCH',     component: MapSearchComponent,      icon: 'map'},
+            { title: 'LOCATIONSEARCH', component: LocationSearchComponent, icon: 'pin' },
+            { title: 'ADDRESSSEARCH',  component: AddressSearchComponent,  icon: 'compass' },
             { title: 'MEETINGLIST',    component: MeetinglistComponent,    icon: 'logo-buffer' },
-			{ title: 'JUSTFORTODAY',   component: JustfortodayComponent,   icon: 'paper' },
+            { title: 'JUSTFORTODAY',   component: JustfortodayComponent,   icon: 'paper' },
             { title: 'DATETIME',       component: DatetimeComponent,       icon: 'clock'},
             { title: 'EVENTS',         component: EventsComponent,         icon: 'calendar'},
-            { title: 'POSTS', 		   component: WordpressPosts,          icon: 'logo-wordpress'},
-	    	{ title: 'SETTINGS',       component: SettingsComponent,       icon: 'settings'},
+            //{ title: 'POSTS', 		   component: WordpressPosts,          icon: 'logo-wordpress'},
+            { title: 'SETTINGS',       component: SettingsComponent,       icon: 'settings'},
             { title: 'CONTACT',        component: ContactComponent,        icon: 'contact'},
             { title: 'ABOUT',          component: AboutComponent,          icon: 'information-circle'}
-		];
-	}
+        ];
+    }
 
-	initializeApp() {
-		this.platform.ready().then(() => {
-			this.statusBar.styleLightContent()
-			this.splashScreen.hide();
-		});
-	}
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleLightContent()
+            this.splashScreen.hide();
+        });
+    }
 
-	openPage(page) {
-		this.menuController.close();
-		this.nav.setRoot(page.component);
-	}
+    openPage(page) {
+        this.menuController.close();
+        this.nav.setRoot(page.component);
+    }
 }
